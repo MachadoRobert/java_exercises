c136a66ffd8c4e3221ee5fd9e49ca7e379e5cd09
Robert Machado <Robert49Machado@gmail.com>
2024-07-09 12:44:12 -0400

    meu primeiro exercício de data e hora no java parte 2

---
+++ src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import date_time_exercises.NY_CurrentTime;
+
+public class App {
+	public static void main (String[]Args) {
+		NY_CurrentTime ny_CurrentTime = new NY_CurrentTime();
+		
+		ny_CurrentTime.ny_CurrentTime();
+	}
+}
